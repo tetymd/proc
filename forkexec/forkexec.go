991d8657446492f3600bace5745663ee1d27d36b
Tetsuya Yamada <yamada.tetsuya258@gmail.com>
2018-12-15 19:59:59 +0900

    add forkexec

---
+++ forkexec/forkexec.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "log"
+    "syscall"
+    "time"
+)
+
+func main() {
+    argv := "./script"
+    args := []string{}
+    attr := &syscall.ProcAttr{}
+
+    pid, err := syscall.ForkExec(argv, args, attr)
+    if err != nil{
+        log.Println(err)
+    }
+    log.Println(pid)
+    time.Sleep(1 * time.Second)
+}
